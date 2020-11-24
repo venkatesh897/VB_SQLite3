263527d0a018ce492e44ad9b3b33c4264bf3908a
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-24 09:02:48 +0530

    Update Vb_sqlite.py

--- Vb_sqlite.py
+++ Vb_sqlite.py
@@ -1,8 +1,8 @@
 #Program which behaves as sqlite3.exe file.
-
 import sqlite3
 import sys
 from printy import printy
+import subprocess
 
 print("SQLite version 3.33.0 2020-08-14 13:23:32")
 print("Enter \".help\" for usage hints.")
@@ -10,7 +10,6 @@ print("Enter \".help\" for usage hints.")
 def get_connection(data_base):
 	connection = sqlite3.connect(data_base)
 	return connection
-
 
 if len(sys.argv) == 2:
 	data_base_name = sys.argv[1]
@@ -24,9 +23,7 @@ else:
 connection = get_connection(data_base_name)
 
 while True:
-
 	query = input("sqlite3> ")
-
 	if query[0] == '.':
 		if query == '.quit':
 			connection.close()
@@ -35,19 +32,14 @@ while True:
 			connection.close()
 			data_base_name = query[6:]
 			connection = get_connection(data_base_name)
-
 		elif query == '.table':
-
 			cursor = connection.execute("SELECT name FROM sqlite_master WHERE type='table';")
-
 			tables = cursor.fetchall()
-
 			for table in tables:
-				print(str(table[0]) + "    " , end = "")
+				print(str(table[0]) + " " , end = "")
 			print("\t")
 		else:
-			print("Query not handled or invalid syntax.")
-			continue
+			subprocess.run(['sqlite3', data_base_name, query])
 	else:
 		try:
 			cursor = connection.execute(query.replace(";", ""))
@@ -64,3 +56,4 @@ while True:
 					if index != length_of_record - 1:
 						print("|", end = "")
 				print("\t")
+
